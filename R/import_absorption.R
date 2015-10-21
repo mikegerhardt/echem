1c237c579a63c03db5fc930c16b0343f07092f77
mikegerhardt <gerhardt@seas.harvard.edu>
2015-10-21 18:10:21 -0400

    adding function to import UV-Vis spectra from Ocean Optics spectrometer

---
+++ R/import_absorption.R
@@ -0,0 +1,10 @@
+import_absorption <- function(filename = file.choose,
+                              data_tag = ">>>>>Begin Spectral Data<<<<<",
+                              columns = c("Wavelength", "Absorption")){
+  filelines <- readLines(filename)
+  beginning_line <- grep(data_tag, filelines)
+  absorption_spectrum <- read.table(textConnection(filelines), 
+                                    skip = beginning_line,
+                                    col.names = columns)
+  return(absorption_spectrum)
+}

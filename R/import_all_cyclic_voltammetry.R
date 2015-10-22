696ad01293d6bad7ee0ce0b713ce2b12356fc3c9
mikegerhardt <gerhardt@seas.harvard.edu>
2015-10-22 10:28:41 -0400

    editing import_all_cyclic_voltammetry so that it doesn't change working directory. I should incorporate this into all import_all functions.

--- R/import_all_cyclic_voltammetry.R
+++ R/import_all_cyclic_voltammetry.R
@@ -1,7 +1,14 @@
 import_all_cyclic_voltammetry <- function(filepath, identifier = ".DTA", ...){
-  home_directory <- getwd()
-  setwd(filepath)
-  cv_filenames <- list.files(pattern = identifier)
-  cv_data <- lapply(cv_filenames, import_cv)
+
+  if(!substr(filepath, nchar(filepath), nchar(filepath)) %in% c("\\", "/")){
+    # This if statement checks to see if filepath ends with "\\" or "/"
+    # since I often forget to add those
+    filepath <- paste(filepath, "\\", sep = "")
+  }
+  
+  cv_filenames <- list.files(pattern = identifier,
+                             path = filepath)
+  cv_filenames_full <- lapply(cv_filenames, function(x) paste(filepath, x, sep = ""))
+  cv_data <- lapply(cv_filenames_full, import_cv)
   return(cv_data)
 }

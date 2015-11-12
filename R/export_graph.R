90da1f6e80fc803bcc9c0b6983ee03bcaa471708
mikegerhardt <gerhardt@seas.harvard.edu>
2015-11-12 15:29:11 -0500

    makes export_graph able to export in more than one size

--- R/export_graph.R
+++ R/export_graph.R
@@ -1,5 +1,5 @@
-export_graph <- function(fname, ggname){
-  png(filename = fname, width = 600, height = 480)
+export_graph <- function(fname, ggname, width  = 600, height = 360){
+  png(filename = fname, width = width, height = height)
   print(ggname)
   dev.off()
 }

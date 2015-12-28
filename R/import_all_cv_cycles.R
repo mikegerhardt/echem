c6961d8f0e69d59c848350fa562eb21b22978c57
mikegerhardt <gerhardt@seas.harvard.edu>
2015-12-28 18:43:48 -0500

    Changes import_all_cv_cycles so that it doesn't change the working directory, and it also gets cycle numbers from the filename instead of assuming a cycle number.

--- R/import_all_cv_cycles.R
+++ R/import_all_cv_cycles.R
@@ -2,7 +2,10 @@
 # These functions import a set of chronocoulometry curves labeled "charge" and
 # "discharge". The "Capacity" one just trims the last charge point off each and
 # is useful if you don't want to look at all the data.
+# This was last edited 12-28-2015 and there were big changes to it.
 
+#' @describeIn import_all_cc_cycles Import all constant voltage cycling curves
+#'   (current vs time)
 
 
 import_all_cv_cycles <- function(filepath, 
@@ -10,46 +13,45 @@ import_all_cv_cycles <- function(filepath,
                                  dischargeid = "discharge",
                                  # getcapacities = TRUE, 
                                  ...){
-  home_directory <- getwd()
-  setwd(filepath)
   
-  all_cycles_list <- list.files(pattern = allcyclesid)
-  discharge_cycles_list <- mixedsort(list.files(pattern = dischargeid))
-  discharge_cycles_location <- all_cycles_list %in% discharge_cycles_list
-  charge_cycles_list <- mixedsort(all_cycles_list[!discharge_cycles_location])
+  all_cycles_list <- list.files(pattern = allcyclesid,
+                                path = filepath,
+                                full.names = TRUE)
+  discharge_cycles_list <- mixedsort(grep(pattern = dischargeid, all_cycles_list, value = TRUE))
+  charge_cycles_list <- mixedsort(grep(pattern = dischargeid, all_cycles_list, value = TRUE, invert = TRUE))
   
   # import all the charge cycles
-  cycle_numbers <- seq(from = 1, to = length(charge_cycles_list), by = 1)
-  charge_cycles_data <- lapply(charge_cycles_list, FUN = import_cv_cycle, ...)
+  charge_cycles_df <- ldply(charge_cycles_list, 
+                            .fun = function(x){
+                              cycledata <- import_cv_cycle(x)
+                              numberplusdigits <- tail(unlist(strsplit(x, split = "#")), 1)
+                              cyclenumber <- as.numeric(regmatches(numberplusdigits, 
+                                                                   regexpr("\\d+",numberplusdigits)))
+                              cycledf <- cbind(cycledata, cycle = cyclenumber)
+                              return(cycledf)
+                            },
+                            ...)
   
   # import all discharge cycles
-  discharge_cycles_data <- lapply(X = discharge_cycles_list, FUN = import_cc_cycle, ...)
+  discharge_cycles_df <- ldply(discharge_cycles_list, 
+                               .fun = function(x){
+                                 cycledata <- import_cc_cycle(x)
+                                 numberplusdigits <- tail(unlist(strsplit(x, split = "#")), 1)
+                                 cyclenumber <- as.numeric(regmatches(numberplusdigits, 
+                                                                      regexpr("\\d+",numberplusdigits)))
+                                 cycledf <- cbind(cycledata, cycle = cyclenumber)
+                                 return(cycledf)
+                               },
+                               ...)
   
-  charge_cycles_data <- mapply(cbind, charge_cycles_data, "cycle" = cycle_numbers, SIMPLIFY = FALSE)
-  discharge_cycles_data <- mapply(cbind, discharge_cycles_data, "cycle" = cycle_numbers, SIMPLIFY = FALSE)
-  
-#   if(getcapacities == TRUE){
-#     # Add a column reporting the charge/discharge capacity if I asked for it (default is to report it)
-#     charge_capacities <- unlist(lapply(charge_cycles_data, 
-#                                        function(x) tail(x, 1)$Q))
-#     
-#     discharge_capacities <- unlist(lapply(discharge_cycles_data,
-#                                           function(x) tail(x, 1)$Q))
-#     
-#     charge_cycles_data <- mapply(cbind, charge_cycles_data, "capacity" = charge_capacities, SIMPLIFY = FALSE)
-#     discharge_cycles_data <- mapply(cbind, discharge_cycles_data, "capacity" = discharge_capacities, SIMPLIFY = FALSE)
-#   }
-  
+
   # set all charge cycles to "Charge" type and all discharge cycles to "Discharge" type
-  charge_cycles_df <- ldply(charge_cycles_data)
   charge_cycles_df$type <- "Charge"
-  discharge_cycles_df <- ldply(discharge_cycles_data)
   discharge_cycles_df$type <- "Discharge"
   
   # smush everything into a data.frame
   
   all_cycles_df <- rbind(charge_cycles_df, discharge_cycles_df)
-  setwd(home_directory)
   return(all_cycles_df)
 }
 

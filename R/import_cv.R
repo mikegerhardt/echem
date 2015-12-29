70bed0a66c09b9282d76220720d851afac2ef30a
mikegerhardt <gerhardt@seas.harvard.edu>
2015-12-29 16:33:03 -0500

    Converts import_cv's documentation to roxygen

--- R/import_cv.R
+++ R/import_cv.R
@@ -1,3 +1,29 @@
+#' Import cyclic voltammogram
+#' 
+#' Import a Gamry .dta file from a cyclic voltammetry experiment.
+#' 
+#' This function should be used to import standard half cell cyclic
+#' voltammogram experiments. By default, it assumes you are using a 3mm
+#' diameter working electrode and a silver/silver chloride reference electrode.
+#'  These can be changed in the function call. If you're trying to import a
+#' cyclic voltammogram performed on a full cell, use \code{import_cell_cv}.
+#' 
+#' @param inputfile File to import
+#' @param cols Columns to import. Must match the column names given by the Gamry
+#'   software.
+#' @param refvoltage Voltage of the reference electrode used in the experiment.
+#'   This function will add this value to the measured voltage in the data file.
+#' @param earea Electrode area. The default value of 0.0765 is the area of a 
+#'   three millimeter disk, in square centimeters.
+#' @param multiplier The function will multiply the current by this after 
+#'   dividing by \code{earea}. This defaults to 1000 so that the output current
+#'   density is in milliamps per square centimeter.
+#' @author Mike Gerhardt
+#' @seealso \code{\link{import_cell_cv}}
+#' @return Returns a data frame with the measured current, the measured voltage
+#'   adjusted by \code{refvoltage}, and the current density, calculated by
+#'   multiplying the measured current by \code{multiplier} and dividing by
+#'   \code{earea}.
 #' @export
 
 import_cv <- function(inputfile = file.choose(), cols = c("Vf", "Im"), 

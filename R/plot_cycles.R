8bda9d3d360d58b8e2c1a96ccb01da9e0efb03e2
mikegerhardt <gerhardt@seas.harvard.edu>
2016-01-12 14:11:37 -0500

    Roxygen comments can only have one space after the #' (not two). Fixes plot_cycles so roxygen works correctly.

--- R/plot_cycles.R
+++ R/plot_cycles.R
@@ -1,20 +1,23 @@
-#'  Plot a set of constant-current charge-discharge cycles
-#'  
-#'  This function takes a set of voltage vs. time curves from a cyclic
-#'  charge-discharge experiment and plots them.
-#'  
-#'  This function is designed to be used on data frames generated with
-#'  \code{import_all_cc_cycles} and may not work otherwise. The function will
-#'  return a ggplot object which can be modified with ggplot methods.
-#'  
-#'  @param inputdf Data frame, typically generated with \code{import_all_cc_cycles}
-#'  
-#'  @author Mike Gerhardt
-#'  @return Returns a ggplot object containing all charge/discharge cycles
-#'    specified by \code{cyclelist}
-#'  
-#'  @export
-#'  @seealso \code{\link{import_all_cc_cycles}}
+#' Plot a set of constant-current charge-discharge cycles
+#' 
+#' This function takes a set of voltage vs. time curves from a cyclic 
+#' charge-discharge experiment and plots them.
+#' 
+#' This function is designed to be used on data frames generated with 
+#' \code{import_all_cc_cycles} and may not work otherwise. The function will 
+#' return a ggplot object which can be modified with ggplot methods.
+#' 
+#' @param inputdf Data frame, typically generated with
+#'   \code{import_all_cc_cycles}
+#' @param cyclelist A sequence of cycle numbers to plot. For example, setting
+#'   \code{cyclelist} to \code{3:6} will plot only cycles 3 through 6. Defaults
+#'   to plot all cycles.
+#' @author Mike Gerhardt
+#' @return Returns a ggplot object containing all charge/discharge cycles 
+#'   specified by \code{cyclelist}
+#'   
+#' @seealso \code{\link{import_all_cc_cycles}}
+#' @export
 
 plot_cycles <- function(inputdf, cyclelist = "all"){
   

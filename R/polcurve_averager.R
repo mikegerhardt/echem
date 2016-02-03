96f4f45af4d41266ceefa79028c3dd97d8bbc9be
mikegerhardt <gerhardt@seas.harvard.edu>
2016-02-03 09:19:02 -0500

    Makes the moving average filter in import_cell_cv its own function

--- R/polcurve_averager.R
+++ R/polcurve_averager.R
@@ -3,6 +3,8 @@
 
 #' @describeIn import_cell_cv Function which averages the top and bottom traces
 #'   of a polarization curve.
+#' @export
+
 polcurve_averager <- function(inputdf, voltageid = "Vf", currentid = "scurrent"){
   
   inputlength <- length(inputdf[, voltageid])
@@ -27,4 +29,22 @@ polcurve_averager <- function(inputdf, voltageid = "Vf", currentid = "scurrent")
   voltagetrace <- inputdf[firstpeak:(firstpeak + length(avgtrace) - 1), voltageid]
   outputdf <- data.frame(avgcurrent = avgtrace, avgvoltage = voltagetrace, avgpower = avgtrace * voltagetrace)
   return(outputdf)
+}
+
+#' @describeIn import_cell_cv Function which smooths out the pulses in the
+#'   polarization curve due to diaphragm pumps
+#' @export
+
+polcurve_smoother <- function(inputdf, rpm){
+  sampletime <- inputdf$T[[2]] - inputdf$T[[1]]
+  
+  pts.per.rot <- 60/(rpm * sampletime) + 1 #number of points in a rotation
+  #plus 1 is for the moving avg filter
+  fparams <- rep(1/pts.per.rot, pts.per.rot)
+  smoothed_current <- as.vector(filter(inputdf$current, fparams, sides = 2))
+  smoothed_power <- smoothed_current * inputdf$Vf
+  smoothed_polcurve <- data.frame(scurrent = smoothed_current,
+                                  svoltage = inputdf$Vf,
+                                  spower = smoothed_power)
+  return(smootehd_polcurve)
 }
